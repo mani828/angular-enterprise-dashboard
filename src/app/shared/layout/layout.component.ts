c53c6d4d4e6ddbba9efd3f4cf8859d807c52c804
Manikanta Aakula <akulamanikanta66@gmail.com>
2026-06-08 16:50:19 +0530

    Implemented crud operation

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -4,7 +4,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -19,5 +20,13 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
+   isMenuOpen = false;
+  constructor(private authService: AuthService, private router: Router){};
+logout(){
+  this.authService.logout();
+  this.router.navigate(['/login']);
+}
+ toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
